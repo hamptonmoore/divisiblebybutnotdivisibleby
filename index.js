66c5aa4de9f7647b7d68b7de807d4197bc8bdd85
Hampton Moore <me@hampton.pw>
2020-02-12 19:42:43 +0100

    Add safety checks

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const divisible = require('divisible')
+const isNumber = require('is-number');
 
 module.exports = function(number, divisorGood, divisorBad) {
-  return divisible(number, divisorGood) && !divisible(number, divisorBad);
+  if (isNumber(number) && isNumber(divisorGood) && isNumber(divisorBad)){
+    return divisible(number, divisorGood) && !divisible(number, divisorBad);
+  } else {
+    throw "All inputs must be numbers";
+  }
 }
